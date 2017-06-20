70737632ddfce657b4eb325327b61a982a01bd6c
Brice Blanch <bricerb@gmail.com>
2017-06-20 09:26:27 -0400

    Commented if statements and rewrote with swith case

--- main.js
+++ main.js
@@ -7,19 +7,39 @@ app.get('/:calc/:num1/:num2', function(req, res) {
   num2 = parseInt(req.params.num2),
   result = 0;
 
-  if (calc === 'add') {
-    result = num1 + num2;
-  } else if (calc === 'subtract') {
-    result = num2 - num1;
-  } else if (calc === 'multiply') {
-    result = num2 * num1;
-  } else if (calc === 'divide') {
-    result = num1 / num2;
-  } else if (calc === 'pow') {
-    result = Math.pow(num1, num2);
-  } else {
-    result = "Incorrect Input...";
+  switch(calc){
+    case 'add':
+      result = num1 + num2;
+      break;
+    case 'subtract':
+      result = num2 - num1;
+      break;
+    case 'divide':
+      result = num1 / num2;
+      break;
+    case 'multiply':
+      result = num1 * num2;
+      break;
+    case 'pow':
+      result = Math.pow(num1, num2);
+      break;
+    default:
+      result = "Incorrect Input...";
+      break;
   }
+  // if (calc === 'add') {
+  //   result = num1 + num2;
+  // } else if (calc === 'subtract') {
+  //   result = num2 - num1;
+  // } else if (calc === 'multiply') {
+  //   result = num2 * num1;
+  // } else if (calc === 'divide') {
+  //   result = num1 / num2;
+  // } else if (calc === 'pow') {
+  //   result = Math.pow(num1, num2);
+  // } else {
+  //   result = "Incorrect Input...";
+  // }
   res.send(result.toString());
 })
 
