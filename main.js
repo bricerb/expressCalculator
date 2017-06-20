cfba8976bb3081d08efa5f3de601c6743b2fb225
Brice Blanch <bricerb@gmail.com>
2017-06-20 00:10:11 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.get('/:calc/:num1/:num2', function(req, res) {
+  var calc = req.params.calc,
+  num1 = parseInt(req.params.num1),
+  num2 = parseInt(req.params.num2),
+  result = 0;
+
+  if (calc === 'add') {
+    result = num1 + num2;
+  } else if (calc === 'subtract') {
+    result = num2 - num1;
+  } else if (calc === 'multiply') {
+    result = num2 * num1;
+  } else if (calc === 'divide') {
+    result = num1 / num2;
+  } else if (calc === 'pow') {
+    result = Math.pow(num1, num2);
+  } else {
+    result = "Incorrect Input...";
+  }
+})
+
+app.listen(3000, function() {
+  console.log('Ready to Calculate');
+})
